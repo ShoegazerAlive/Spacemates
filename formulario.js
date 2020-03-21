a0bdccf18a6521d301611011ff1f64fca7c24622
Diego Reyes <diegoreyes@MacBook-Air-de-Diego.local>
2020-03-21 17:53:06 -0600

    Proyecto web Space

---
+++ formulario.js
@@ -0,0 +1,7 @@
+var scritp_url = "https://script.google.com/macros/s/AKfycbwBqvbXNpq9eLzxeqZuwvkmSWQnaiaMCDWXtxLvFtlF7ikUz2E/exec";
+function insert_value(){
+    var nombre = $('#nombre').val();
+    var email = $('#email').val();
+    var ciudad = $('#ciudad').val();
+
+}
