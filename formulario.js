d414cbba148a0d500a5102d15bbc3be475a0da19
Diego Reyes <diegoreyes@MacBook-Air-de-Diego.local>
2020-03-23 18:27:50 -0600

    Actualización Spacemates

--- formulario.js
+++
@@ -1,7 +0,0 @@
-var scritp_url = "https://script.google.com/macros/s/AKfycbwBqvbXNpq9eLzxeqZuwvkmSWQnaiaMCDWXtxLvFtlF7ikUz2E/exec";
-function insert_value(){
-    var nombre = $('#nombre').val();
-    var email = $('#email').val();
-    var ciudad = $('#ciudad').val();
-
-}
